93197976a26a3154c7cb2aa585099a2af8994dd7
jim.hsu <jim.hsu@deanshoes.com>
2023-08-31 15:40:29 +0800

    作品預覽頁面，跳轉功能初步完成

--- src/components/Artworks/Artworks.js
+++ src/components/Artworks/Artworks.js
@@ -3,9 +3,9 @@ import { Container, Row, Col, ListGroup } from "react-bootstrap";
 import ArtworkCards from "./ArtworkCards";
 import Particle from "../Particle";
 import FakeImage from "../../Assets/Artworks/Sketch/FakeImage.png";
-import Scroll from 'react-scroll'
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
-const ScrollLink = Scroll.ScrollLink
+
 
 function Artworks() {
   return (
@@ -19,28 +19,22 @@ function Artworks() {
           Here are a few artWorks I've worked on recently.
         </p>
 
+
         <ListGroup horizontal>
-          <ListGroup.Item className="listgroup-artworks-item" action href="">      <ScrollLink 
-        to="anime-style" 
-        spy={true} 
-        smooth={true} 
-        duration={500} 
-     
-      >
-        日系插畫設計
-      </ScrollLink>       </ListGroup.Item>
-          <ListGroup.Item className="listgroup-artworks-item" action href=""><span><strong  className="nav-link">場景設計</strong></span></ListGroup.Item>
+
+          <ListGroup.Item className="listgroup-artworks-item" > <Link activeClass="active" className="anime-style" to="anime-style" spy={true} smooth={true} duration={500} >日系插畫設計</Link>    </ListGroup.Item>
+          <ListGroup.Item className="listgroup-artworks-item" action href="">  <Link activeClass="active" className="concept-art" to="concept-art" spy={true} smooth={true} duration={500} >場景設計</Link></ListGroup.Item>
           <ListGroup.Item className="listgroup-artworks-item" action href="">
-            <span><strong  className="nav-link">卡片設計</strong></span></ListGroup.Item>
+            <Link activeClass="active" className="card-design" to="card-design" spy={true} smooth={true} duration={500} >卡片設計</Link> </ListGroup.Item>
           <ListGroup.Item className="listgroup-artworks-item" action href="">
-            <span><strong  className="nav-link">鉛筆素描</strong></span></ListGroup.Item>
+            <Link activeClass="active" className="pencel-sketch" to="pencel-sketch" spy={true} smooth={true} duration={500} > 鉛筆素描</Link> </ListGroup.Item>
           <ListGroup.Item className="listgroup-artworks-item" action href="">
-            <span><strong  className="nav-link">電繪素描</strong></span></ListGroup.Item>
+            <Link activeClass="active" className="digital-sketch" to="digital-sketch" spy={true} smooth={true} duration={500} >   電繪素描</Link> </ListGroup.Item>
         </ListGroup>
 
 
         <Row>
-          <h1 className="purple" id = "anime-style">日系插畫設計</h1>
+          <Element name="anime-style" className="Element"><h1 className="purple" id="anime-style">日系插畫設計</h1></Element>
         </Row>
         <Row>
           <Col md={4}><ArtworkCards
@@ -100,7 +94,7 @@ function Artworks() {
         </Row>
 
         <Row>
-          <h1 className="purple">卡片設計</h1>
+          <Element name="card-design" className="Element">    <h1 className="purple">卡片設計</h1> </Element>
         </Row>
         <Row>
           <Col md={4}><ArtworkCards
@@ -144,7 +138,8 @@ function Artworks() {
         </Row>
 
         <Row>
-          <h1 className="purple">場景設計</h1>
+          <Element name="concept-art" className="Element">    <h1 className="purple">場景設計</h1></Element>
+
         </Row>
         <Row>
           <Col md={4}><ArtworkCards
@@ -188,7 +183,7 @@ function Artworks() {
           </Col>
         </Row>
         <Row>
-          <h1 className="purple">鉛筆素描</h1>
+          <Element name="pencel-sketch" className="Element">    <h1 className="purple">鉛筆素描</h1> </Element>
         </Row>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
@@ -225,7 +220,7 @@ function Artworks() {
           </Col>
         </Row>
         <Row>
-          <h1 className="purple">電繪素描</h1>
+          <Element name="digital-sketch" className="Element">       <h1 className="purple">電繪素描</h1> </Element>
         </Row>
         <Row>
           <Col md={4} className="project-card">
