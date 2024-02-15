8e02d89bee30ce8c9fb9c25e7082e23a974b9b35
SPP <hatsunemiku1144@gmail.com>
2024-02-15 22:16:00 +0800

    新增圖片；修圖片背景透明度問題

--- src/components/Artworks/Artworks.js
+++ src/components/Artworks/Artworks.js
@@ -3,6 +3,14 @@ import { Container, Row, Col, ListGroup } from "react-bootstrap";
 import ArtworkCards from "./ArtworkCards";
 import Particle from "../Particle";
 import FakeImage from "../../Assets/Artworks/Sketch/FakeImage.png";
+import KANNA from "../../Assets/Artworks/ILLUSTATION/KANNA.jpg";
+import CHACHAMARU from "../../Assets/Artworks/ILLUSTATION/CHACHAMARU.jpg";
+import HOSHINO from "../../Assets/Artworks/ILLUSTATION/HOSHINO.jpg";
+import YUI from "../../Assets/Artworks/ILLUSTATION/YUI.png";
+import MOKOU from "../../Assets/Artworks/ILLUSTATION/MOKOU.jpg";
+import Skectch1 from "../../Assets/Artworks/Sketch/Skectch1.jpg";
+import Mai from "../../Assets/Artworks/CharacterDesign/Mai.jpg";
+import Pen from "../../Assets/Artworks/CharacterDesign/Pen.jpg";
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 
@@ -22,114 +30,75 @@ function Artworks() {
 
         <ListGroup horizontal>
 
-          <ListGroup.Item className="listgroup-artworks-item" > <Link activeClass="active" className="anime-style" to="anime-style" spy={true} smooth={true} duration={500} >日系插畫設計</Link>    </ListGroup.Item>
-          <ListGroup.Item className="listgroup-artworks-item" action href="">  <Link activeClass="active" className="concept-art" to="concept-art" spy={true} smooth={true} duration={500} >場景設計</Link></ListGroup.Item>
+          <ListGroup.Item className="listgroup-artworks-item" action href="">  <Link activeClass="active" className="concept-art" to="concept-art" spy={true} smooth={true} duration={500} >ILLUSTATION</Link></ListGroup.Item>
+          <ListGroup.Item className="listgroup-artworks-item" action href="">  <Link activeClass="active" className="concept-art" to="concept-art" spy={true} smooth={true} duration={500} >CharacterDesign</Link></ListGroup.Item>
           <ListGroup.Item className="listgroup-artworks-item" action href="">
-            <Link activeClass="active" className="card-design" to="card-design" spy={true} smooth={true} duration={500} >卡片設計</Link> </ListGroup.Item>
+            <Link activeClass="active" className="card-design" to="card-design" spy={true} smooth={true} duration={500} >Card-design</Link> </ListGroup.Item>
           <ListGroup.Item className="listgroup-artworks-item" action href="">
-            <Link activeClass="active" className="pencel-sketch" to="pencel-sketch" spy={true} smooth={true} duration={500} > 鉛筆素描</Link> </ListGroup.Item>
-          <ListGroup.Item className="listgroup-artworks-item" action href="">
-            <Link activeClass="active" className="digital-sketch" to="digital-sketch" spy={true} smooth={true} duration={500} >   電繪素描</Link> </ListGroup.Item>
+            <Link activeClass="active" className="pencel-sketch" to="pencel-sketch" spy={true} smooth={true} duration={500} > Skectch</Link> </ListGroup.Item>
         </ListGroup>
 
 
         <Row>
-          <Element name="anime-style" className="Element"><h1 className="purple" id="anime-style">日系插畫設計</h1></Element>
+          <Element name="anime-style" className="Element"><h1 className="purple" id="anime-style">插畫作品</h1></Element>
         </Row>
         <Row>
-          <Col md={4}><ArtworkCards
-            imgSrc={FakeImage}
+          <Col md={12}><ArtworkCards
+            imgSrc={KANNA}
             artworkTitle="Title2"
             desc="desc2"
             artworkLink=""
           /></Col>
-          <Col md={4} className="project-card">
+          <Col md={6} className="project-card">
             <ArtworkCards
-              imgSrc={FakeImage}
+              imgSrc={MOKOU}
               artworkTitle="Title2"
               desc="desc2"
               artworkLink=""
             />
           </Col>
-          <Col md={4} className="project-card">
+          <Col md={6} className="project-card">
             <ArtworkCards
-              imgSrc={FakeImage}
+              imgSrc={YUI}
               artworkTitle="Title2"
               desc="desc2"
               artworkLink=""
             />
           </Col>
-          <Col md={4} className="project-card">
+          <Col md={6} className="project-card">
             <ArtworkCards
-              imgSrc={FakeImage}
+              imgSrc={HOSHINO}
               artworkTitle="Title2"
               desc="desc2"
               artworkLink=""
             />
           </Col>
-          <Col md={4} className="project-card">
+          <Col md={6} className="project-card">
             <ArtworkCards
-              imgSrc={FakeImage}
+              imgSrc={CHACHAMARU}
               artworkTitle="Title2"
               desc="desc2"
               artworkLink=""
             />
           </Col>
-          <Col md={4} className="project-card">
-            <ArtworkCards
-              imgSrc={FakeImage}
-              artworkTitle="Title2"
-              desc="desc2"
-              artworkLink=""
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ArtworkCards
-              imgSrc={FakeImage}
-              artworkTitle="Title2"
-              desc="desc2"
-              artworkLink=""
-            />
-          </Col>
+
         </Row>
 
         <Row>
-          <Element name="card-design" className="Element">    <h1 className="purple">卡片設計</h1> </Element>
+          <Element name="card-design" className="Element">    <h1 className="purple">角色設計</h1> </Element>
         </Row>
         <Row>
-          <Col md={4}><ArtworkCards
-            imgSrc={FakeImage}
-            artworkTitle="Title2"
-            desc="desc2"
-            artworkLink=""
-          /></Col>
-          <Col md={4} className="project-card">
+          <Col md={7} className="project-card">
             <ArtworkCards
-              imgSrc={FakeImage}
+              imgSrc={Mai}
               artworkTitle="Title2"
               desc="desc2"
               artworkLink=""
             />
           </Col>
-          <Col md={4} className="project-card">
+          <Col md={5} className="project-card">
             <ArtworkCards
-              imgSrc={FakeImage}
-              artworkTitle="Title2"
-              desc="desc2"
-              artworkLink=""
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ArtworkCards
-              imgSrc={FakeImage}
-              artworkTitle="Title2"
-              desc="desc2"
-              artworkLink=""
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ArtworkCards
-              imgSrc={FakeImage}
+              imgSrc={Pen}
               artworkTitle="Title2"
               desc="desc2"
               artworkLink=""
@@ -183,12 +152,12 @@ function Artworks() {
           </Col>
         </Row>
         <Row>
-          <Element name="pencel-sketch" className="Element">    <h1 className="purple">鉛筆素描</h1> </Element>
+          <Element name="pencel-sketch" className="Element">    <h1 className="purple">鉛筆素描/速寫</h1> </Element>
         </Row>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ArtworkCards
-              imgSrc={FakeImage}
+              imgSrc={Skectch1}
               artworkTitle="Title1"
               desc="desc1"
               artworkLink="https://www.google.com/"
@@ -219,43 +188,7 @@ function Artworks() {
             />
           </Col>
         </Row>
-        <Row>
-          <Element name="digital-sketch" className="Element">       <h1 className="purple">電繪素描</h1> </Element>
-        </Row>
-        <Row>
-          <Col md={4} className="project-card">
-            <ArtworkCards
-              imgSrc={FakeImage}
-              artworkTitle="Title1"
-              desc="desc1"
-              artworkLink="https://www.google.com/"
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ArtworkCards
-              imgSrc={FakeImage}
-              artworkTitle="Title2"
-              desc="desc2"
-              artworkLink=""
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ArtworkCards
-              imgSrc={FakeImage}
-              artworkTitle="Title2"
-              desc="desc2"
-              artworkLink=""
-            />
-          </Col>
-          <Col md={4} className="project-card">
-            <ArtworkCards
-              imgSrc={FakeImage}
-              artworkTitle="Title2"
-              desc="desc2"
-              artworkLink=""
-            />
-          </Col>
-        </Row>
+
 
 
 
