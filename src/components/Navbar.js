02265e1644eebec27ca2cc6c4b1970617c5d81c4
SPP <hatsunemiku1144@gmail.com>
2024-02-29 09:27:23 +0800

    change navbar href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
+        <Navbar.Brand href="/Artportfolio" className="d-flex">
           <img src={logo} className="img-fluid logo" alt="brand" />
         </Navbar.Brand>
         <Navbar.Toggle
